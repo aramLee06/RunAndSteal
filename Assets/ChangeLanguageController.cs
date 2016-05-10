1217cc8692a6c591a725567e5230ca5d5efac29b
Roxie <sjkim@cspmedia.net>
2016-05-10 18:07:08 +0900

    language button event connect

---
+++ Assets/ChangeLanguageController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeLanguageController : MonoBehaviour {
+	private CommonLang commonLang;
+
+	// Use this for initialization
+	void Start () {
+		commonLang = CommonLang.instance;
+	}
+
+	public void OnKoreanButton(){
+		commonLang.Language = LanguageType.KOR;
+	}
+
+	public void OnEnglishButton(){
+		commonLang.Language = LanguageType.ENG;
+	}
+
+	public void OnChineseButton(){
+		commonLang.Language = LanguageType.CHI;
+	}
+
+}

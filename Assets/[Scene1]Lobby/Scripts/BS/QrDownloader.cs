3663964f746920506b000464f932a3895aecf377
Juhee Lim <jhlim@cspmedia.net>
2016-06-01 15:34:35 +0900

    QR code 이미지다운받아서띄움

---
+++ Assets/[Scene1]Lobby/Scripts/BS/QrDownloader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class QrDownloader : MonoBehaviour {
+
+	public string url = "http://cdn.playgameparty.com/code.png";
+
+	IEnumerator Start () {
+		
+		WWW www = new WWW(url);
+		yield return www;
+		gameObject.GetComponent<RawImage> ().texture = www.texture;
+	}
+
+}
+
+
